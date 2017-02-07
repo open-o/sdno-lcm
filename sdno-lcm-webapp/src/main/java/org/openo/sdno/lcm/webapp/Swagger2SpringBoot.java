793d77dee47380387881927803796a86108983ce
Mark OKeeffe <mark.o.keeffe@huawei.com>
2017-02-07 17:27:42 +0000

    Cobertura config
    Issue-id: SDNO-1223
    Change-Id: I444c851a68bd1698552cc8ec1b8e503999af3a40
    Signed-off-by: Mark OKeeffe <mark.o.keeffe@huawei.com>

--- sdno-lcm-webapp/src/main/java/org/openo/sdno/lcm/webapp/Swagger2SpringBoot.java
+++ sdno-lcm-webapp/src/main/java/org/openo/sdno/lcm/webapp/Swagger2SpringBoot.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.openo;
+package org.openo.sdno.lcm.webapp;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
