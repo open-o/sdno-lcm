d28c70ef67069a8e68716cdaffdf43bd69836ce1
Xiuchao Wu <xiuchao.wu@huawei.com>
2017-02-27 18:46:18 +0000

    Mapper implementation and unit-tests
    Issue-id: SDNO-1340
    
    Change-Id: Iba7e8bc7c9502b8e5aa116711bc4b4273189f530
    Signed-off-by: Xiuchao Wu <xiuchao.wu@huawei.com>

--- sdno-lcm-engine/src/main/java/org/openo/sdno/lcm/dispatcher/impl/RequestBodyMapperImpl.java
+++ sdno-lcm-engine/src/main/java/org/openo/sdno/lcm/dispatcher/impl/RequestBodyMapperImpl.java
@@ -20,11 +20,18 @@ import org.openo.sdno.lcm.model.workplan.WorkItem;
 import io.swagger.models.Swagger;
 import org.openo.sdno.lcm.templatemodel.service.Node;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.util.Iterator;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 import org.openo.sdno.lcm.util.SwaggerUtils;
+import org.openo.sdno.lcm.exception.LcmInternalException;
 
 import static com.google.common.base.Preconditions.*;
 import org.springframework.stereotype.Component;
@@ -33,9 +40,13 @@ import org.openo.sdno.lcm.dispatcher.RequestBodyMapper;
 
 @Component
 public class RequestBodyMapperImpl implements RequestBodyMapper {
+    private final Logger logger = Logger.getLogger("RequestBodyMapperImpl");
+
     private JsonNodeFactory nodeFactory;
 
-    //constructor
+    /**
+     * Constructor
+     */
     public RequestBodyMapperImpl() {
         nodeFactory = JsonNodeFactory.instance;
     }
@@ -54,15 +65,25 @@ public class RequestBodyMapperImpl implements RequestBodyMapper {
     public JsonNode map(final WorkItem workItem) {
         checkNotNull(workItem, "Work item cannot be null.");
 
+        //prepare input parameters needed by mapping();
         JsonNode mapperSpec = workItem.getMapperSpec();
         String modelName = SwaggerUtils.getBodyModelNameFromSwagger(workItem.getSwaggerSpec(), workItem.getApiUrl(), workItem.getMethod());
 
         JsonNode nodeProperties = workItem.getNode().getProperties();
 
-        JsonNode resultNode = nodeFactory.objectNode();
+        ObjectNode objectNode = nodeFactory.objectNode();  //the output
 
-        mapping(nodeProperties, mapperSpec, modelName, resultNode);
+        //real workhorse
+        mapping(nodeProperties, mapperSpec, modelName, objectNode);
 
+        //return as JsonNode
+        JsonNode resultNode = null;
+        try {
+            resultNode = (JsonNode) new ObjectMapper().readTree(objectNode.toString());
+        } catch(IOException e) {
+            logger.severe(String.format("Unexpected error happens when converting ObjectNode to JsonNode. ObjectNode: %s", objectNode.toString()));
+            throw new LcmInternalException(e.toString());
+        }
         return resultNode;
     }
 
@@ -75,20 +96,102 @@ public class RequestBodyMapperImpl implements RequestBodyMapper {
      * @param properties the properties in node instance
      * @param mapperSpec the whole specification for the mapper
      * @param modelName teh name of the model/type to be constructed
-     * @param resultNode used to hold the constructed JSON. act as the output.
+     * @param objectNode used to hold the constructed JSON. act as the output.
      */
-    protected void mapping(JsonNode properties, JsonNode mapperSpec, String modelName, JsonNode resultNode) {
+    protected void mapping(JsonNode properties, JsonNode mapperSpec, String modelName, ObjectNode objectNode) {
         checkNotNull(properties, "Properties (node instance) cannot be null.");
         checkNotNull(mapperSpec, "Mapper specification cannot be null.");
-        checkNotNull(resultNode, "Result node cannot be null.");
+        checkNotNull(objectNode, "object node (output) cannot be null.");
         checkArgument((modelName!=null)&&(modelName.length()>0), "Model name cannot be null or empty.");
 
         //get related mapping rules
-        JsonNode mappingRules = mapperSpec.get(modelName);
-        //For each rule (Key-Value pair. Note that key is the field name in the data model and value is the property name in node instance.):
-            // Check the value and format of the key.  Format is described in  SDNO-1249.
-            // If the key is a basic type, simply copy node property (property name is specified in the value of the mapping rule) to the data model / Json object to be returned by Mapper.
-            // If the key is one array type, use a loop to process each item.
-            // If the key is a complex type, call mapping() recursively to do the mapping.
+        ObjectNode mappingRules = null;
+        try {
+            mappingRules = (ObjectNode) new ObjectMapper().readTree((mapperSpec.get(modelName)).toString());
+        } catch(IOException e) {
+            logger.severe(String.format("Unexpected error happens when converting mapper specification to ObjectNode. Mapper Spec: %s", mapperSpec.get(modelName).toString()));
+            throw new LcmInternalException(e.toString());
+        }
+
+        //process mapping rules one by one
+        Iterator<String> ruleNameIterator = mappingRules.fieldNames();
+        while(ruleNameIterator.hasNext()) {
+            //construct mapping rule class
+            String keyStr = ruleNameIterator.next();
+            String valueStr = (mappingRules.get(keyStr)).asText();
+            RequestBodyMappingRule rule = new RequestBodyMappingRule(keyStr, valueStr);
+
+            //get corresponding value node in properties
+            String apiFieldName = rule.getKeyName();
+            String nodePropertyName = rule.getValueName();
+            JsonNode valueNode = (properties.get(nodePropertyName));
+
+            //skip if properties has no such value (optional property)
+            if(null == valueNode) continue;
+
+            //variables for holding temporary results
+            ArrayNode arrayNode = null;
+            ArrayNode valueArray = null;
+            ObjectNode newObjectNode = null;
+            JsonNode value = null;
+            switch(rule.getKeyType()) {
+                case SCALAR:  //mapping scalar
+                    value = valueNode.get("value");
+                    objectNode.put(apiFieldName, value);
+                    break;
+                case OBJECT: //mapping object
+                    //create new object node for sub-model and add to the node for output.
+                    newObjectNode = nodeFactory.objectNode();
+                    objectNode.put(apiFieldName, newObjectNode);
+                    mapping(valueNode, mapperSpec, rule.getKeyObjectModelName(), newObjectNode);
+
+                    break;
+                case SCALAR_ARRAY: //mapping scalar array
+                    //create new array node and add to the node for output.
+                    arrayNode = nodeFactory.arrayNode();
+                    objectNode.put(apiFieldName, arrayNode);
+
+                    //convert valueNode to array node
+                    try {
+                        valueArray = (ArrayNode) new ObjectMapper().readTree(valueNode.toString());
+                    } catch(IOException e) {
+                        logger.severe(String.format("Unexpected error happens when converting value node to ArrayNode. value node: %s", valueNode.toString()));
+                        throw new LcmInternalException(e.toString());
+                    }
+
+                    //mapping array items one by one
+                    for(int i=0; i<valueArray.size(); i++) {
+                        JsonNode itemNode = valueArray.get(i);
+                        value = itemNode.get("value");
+                        arrayNode.add(value);
+                    }
+
+                    break;
+                case OBJECT_ARRAY: //mapping object array
+                    //create new array node and add to the node for output.
+                    arrayNode = nodeFactory.arrayNode();
+                    objectNode.put(apiFieldName, arrayNode);
+
+                    //convert valueNode to array node
+                    try {
+                        valueArray = (ArrayNode) new ObjectMapper().readTree(valueNode.toString());
+                    } catch(IOException e) {
+                        logger.severe(String.format("Unexpected error happens when converting value node to ArrayNode. value node: %s", valueNode.toString()));
+                        throw new LcmInternalException(e.toString());
+                    }
+
+                    //mapping array items (sub-model) one by one
+                    for(int i=0; i<valueArray.size(); i++) {
+                        JsonNode itemNode = valueArray.get(i);
+                        newObjectNode = nodeFactory.objectNode();
+                        arrayNode.add(newObjectNode);
+                        mapping(itemNode, mapperSpec, rule.getKeyObjectModelName(), newObjectNode);
+                    }
+
+                    break;
+                default:
+                    throw new LcmInternalException(String.format("Unknown API Field Type is found in mapping rule. mapping key: %s", keyStr));
+            }
+        }
     }
 }
--- sdno-lcm-engine/src/test/java/org/openo/sdno/lcm/dispatcher/impl/RequestBodyMapperImplTest.java
+++ sdno-lcm-engine/src/test/java/org/openo/sdno/lcm/dispatcher/impl/RequestBodyMapperImplTest.java
@@ -16,22 +16,105 @@
 
 package org.openo.sdno.lcm.dispatcher.impl;
 
-import org.openo.sdno.lcm.model.workplan.WorkItem;
-import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.commons.io.FileUtils;
+import java.nio.charset.Charset;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import static org.testng.Assert.*;
 
+import org.openo.sdno.lcm.dispatcher.RequestBodyMapper;
+
+/**
+ * This class test RequestBodyMapperImpl based on "node-properties-test.json" and "mapper-test.tsmap".
+ * Below are the expected JSON nodes to be returned.
+ *   ModelWithScalar:  {"id":"MyId","name":"MyName","size":1,"enabled":true}
+ *   ModelWithScalarArray:  {"id":"MyId","numbers":[1,2]}
+ *   ModelWithSubModel:  {"id":"MyId","address":{"ip":"10.0.0.1","port":80}}
+ *   ModelWithObjectArray:  {"id":"MyId","rules":[{"protocol":"Protocol1","policy":"Policy1"},{"protocol":"Protocol2","policy":"Policy2"}]}
+*/
 
 @Test(groups = {"sdno-lcm-unit"})
 @ContextConfiguration(locations = {"classpath:spring-test-config.xml"})
 public class RequestBodyMapperImplTest extends AbstractTestNGSpringContextTests {
+    private RequestBodyMapperImpl mapper;
+    private JsonNode properties;
+    private JsonNode mapperSpec;
+    private JsonNodeFactory nodeFactory;
+
+    @BeforeClass
+    public void initialize() {
+        nodeFactory = JsonNodeFactory.instance;
+        mapper = new RequestBodyMapperImpl();
+
+        try {
+            String propertyContent = FileUtils.readFileToString(FileUtils.getFile("src","test", "resources", "node-properties-test.json"), Charset.defaultCharset());
+            properties = (JsonNode) new ObjectMapper().readTree(propertyContent);
+
+            String mapperSpecContent = FileUtils.readFileToString(FileUtils.getFile("src","test", "resources", "mapper-test.tsmap"), Charset.defaultCharset());
+            mapperSpec = (JsonNode) new ObjectMapper().readTree(mapperSpecContent);
+        }  catch(IOException e) {
+            assertTrue(false, "Mapper specification and/or node properties files are missed or the content cannot be parsed successfully.");
+        }
+    }
+
 
     @Test
-    public void testWithEmptyMapSpec() {
+    public void testModelWithScalar() {
+        ObjectNode objectNode = nodeFactory.objectNode();
+        mapper.mapping(properties, mapperSpec, "ModelWithScalar", objectNode);
+
+        assertEquals(objectNode.get("id").asText(), "MyId", "Unexpected value is returned!");
+        assertEquals(objectNode.get("name").asText(), "MyName", "Unexpected value is returned!");
+        assertEquals(objectNode.get("size").asInt(), 1, "Unexpected value is returned!");
+        assertTrue(objectNode.get("enabled").asBoolean(), "Unexpected value is returned!");
+
         return;
     }
 
+    @Test
+    public void testModelWithScalarArray() {
+        ObjectNode objectNode = nodeFactory.objectNode();
+        mapper.mapping(properties, mapperSpec, "ModelWithScalarArray", objectNode);
+
+        assertEquals(objectNode.get("id").asText(), "MyId", "Unexpected value is returned!");
+        assertEquals(objectNode.get("numbers").get(0).asInt(), 1, "Unexpected value is returned!");
+        assertEquals(objectNode.get("numbers").get(1).asInt(), 2, "Unexpected value is returned!");
+
+        return;
+    }
+
+    @Test
+    public void testModelWithSubModel() {
+        ObjectNode objectNode = nodeFactory.objectNode();
+        mapper.mapping(properties, mapperSpec, "ModelWithSubModel", objectNode);
+
+        assertEquals(objectNode.get("id").asText(), "MyId", "Unexpected value is returned!");
+        assertEquals(objectNode.get("address").get("ip").asText(), "10.0.0.1", "Unexpected value is returned!");
+        assertEquals(objectNode.get("address").get("port").asInt(), 80, "Unexpected value is returned!");
+
+        return;
+    }
+
+    @Test
+    public void testModelWithObjectArray() {
+        ObjectNode objectNode = nodeFactory.objectNode();
+        mapper.mapping(properties, mapperSpec, "ModelWithObjectArray", objectNode);
+
+        assertEquals(objectNode.get("id").asText(), "MyId", "Unexpected value is returned!");
+        assertEquals(objectNode.get("rules").get(0).get("protocol").asText(), "Protocol1", "Unexpected value is returned!");
+        assertEquals(objectNode.get("rules").get(0).get("policy").asText(), "Policy1", "Unexpected value is returned!");
+        assertEquals(objectNode.get("rules").get(1).get("protocol").asText(), "Protocol2", "Unexpected value is returned!");
+        assertEquals(objectNode.get("rules").get(1).get("policy").asText(), "Policy2", "Unexpected value is returned!");
+
+        return;
+    }
 }
