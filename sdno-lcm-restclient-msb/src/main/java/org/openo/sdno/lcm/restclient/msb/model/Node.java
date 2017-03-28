cdac5e454a49bc9a9b5267bbdd2bc34faa47c63a
Venu Nanditha Jampani <venunanditha568@gmail.com>
2017-03-28 21:13:36 +0530

    Update licence headers on generated code.
    issue_id:SDNO-1482
    
    Change-Id: Ide4c0b3dd537bc71a4bcc75fd71711eb8b621885
    Signed-off-by: Venu Nanditha Jampani <venunanditha568@gmail.com>

--- sdno-lcm-restclient-msb/src/main/java/org/openo/sdno/lcm/restclient/msb/model/Node.java
+++ sdno-lcm-restclient-msb/src/main/java/org/openo/sdno/lcm/restclient/msb/model/Node.java
@@ -20,6 +20,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Copyright 2016-2017 ZTE Corporation.
+ * Modifications Copyright 2017 Huawei Technologies Co., Ltd.
  */
 
 
